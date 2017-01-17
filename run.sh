2ab956bb11942e70e502ed5aab883a7847f9d710
Troy Harvey <troy.harvey@generationtux.com>
2017-01-17 12:26:15 -0500

    Add extra feedback during replica check failure

--- run.sh
+++ run.sh
@@ -4,37 +4,39 @@
 # of available replicas.
 #
 # Arguments:
+#   timeout     Seconds. How long should we wait for the replicas to be available.
 #   app         The value of metadata.labels.app from the deployment config. E.g. prod-products or qa-products.
 #   namespace   Default: default. Namespace where the deployment lives. E.g. prod or default.
 #
 # Usage:
-#   ./deploy/lib/scripts/kubectl-replica-check.sh qa-products
-#   ./deploy/lib/scripts/kubectl-replica-check.sh prod-products prod
-
-
+#
+#    - gentux/validate-replicas:
+#        timeout: 120
+#        app: staging-products
+#        namespace: staging
 
 if [ $(which kubectl 2>/dev/null | grep -v "not found" | wc -l) -eq 0 ] ; then
     echo "kubectl is not installed" &&
     exit 1
 fi
 
-info "kubectl is installed"
-
 TIMEOUT_FLAG=0
 
 while : ; do
-    if [ TIMEOUT_FLAG -eq $WERCKER_VALIDATE_REPLICAS_TIMEOUT ] ; then
-        break
+    if [ $TIMEOUT_FLAG -eq $WERCKER_VALIDATE_REPLICAS_TIMEOUT ] ; then
+        echo "Kubernetes replica check failed."
+        echo "Expected replicas: $REPLICAS, Available replicas: $AVAILABLE_REPLICAS"
+        exit 1
     fi
 
-    REPLICAS="$(kubectl get -o jsonpath='{.items[0].status.replicas}' deployments --selector=app=$WERCKER_VALIDATE_REPLICAS_APP --namespace=$WERCKER_VALIDATE_REPLICAS_NAMESPACE:-default)"
-    AVAILABLE_REPLICAS="$(kubectl get -o jsonpath='{.items[0].status.availableReplicas}' deployments --selector=app=$WERCKER_VALIDATE_REPLICAS_APP --namespace=$WERCKER_VALIDATE_REPLICAS_NAMESPACE:-default)"
+    REPLICAS="$(kubectl get -o jsonpath='{.items[0].status.replicas}' deployments --selector=app=$WERCKER_VALIDATE_REPLICAS_APP --namespace=${WERCKER_VALIDATE_REPLICAS_NAMESPACE:-default})"
+    AVAILABLE_REPLICAS="$(kubectl get -o jsonpath='{.items[0].status.availableReplicas}' deployments --selector=app=$WERCKER_VALIDATE_REPLICAS_APP --namespace=${WERCKER_VALIDATE_REPLICAS_NAMESPACE:-default})"
 
-    echo "Expected replicas: $REPLICAS, Available replicas: $AVAILABLE_REPLICAS"
     ((TIMEOUT_FLAG++))
+    sleep .5
 
     [[ $REPLICAS -ne $AVAILABLE_REPLICAS ]] || break
 done
 
+echo "Expected replicas: $REPLICAS, Available replicas: $AVAILABLE_REPLICAS"
 exit 0
-
